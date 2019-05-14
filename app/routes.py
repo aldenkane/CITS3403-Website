b489da7e0fbbe437d6c0d0d6ddf7b5a9820ee498
Alden Kane <aldenkane1@Aldens-MacBook-Pro-2.local>
2019-05-15 00:39:10 +0800

    Beginning of Login Form

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
-from flask import render_template
+from flask import render_template, url_for, flash, redirect, send_from_directory
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
+
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'} #Pseudo User noise. Might delete with actual user functionality
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
