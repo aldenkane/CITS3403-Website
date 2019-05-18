7b672aeed8efaae745a0f063e252c43364966857
Alden Kane <aldenkane1@Aldens-MacBook-Pro-2.local>
2019-05-18 20:08:08 +0800

    Databases Set Up for Polling

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP = CITS3403-Website.py
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
 @login.user_loader
 def load_user(id):
@@ -20,3 +21,41 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+# Base model for Polling Options
+class Base(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+# Model for poll topics
+class Topics(Base):
+    title = db.Column(db.String(500))
+
+    # Display the object
+    def __repr__(self):
+        return self.title
+
+# Model for poll options
+class Options(Base):
+    name = db.Column(db.String(200))
+
+# Polls model for voting
+class Polls(Base):
+
+    # Columns declaration
+    topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'))
+    option_id = db.Column(db.Integer, db.ForeignKey('options.id'))
+    vote_count = db.Column(db.Integer, default=0)
+    status = db.Column(db.Boolean) # to mark poll as open or closed
+
+    # Relationship declaration (makes it easier for us to access the polls model
+    # from the other models it's related to)
+    topic = db.relationship('Topics', foreign_keys=[topic_id], backref=db.backref('options', lazy='dynamic'))
+    option = db.relationship('Options',foreign_keys=[option_id])
+
+    def __repr__(self):
+        # a user friendly way to view our objects in the terminal
+        return self.option.name
