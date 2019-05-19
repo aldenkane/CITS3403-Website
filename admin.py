cb0db35be7e2f42d1b2632cb0834b5a505d6a452
Alden Kane <aldenkane1@Aldens-MacBook-Pro-2.local>
2019-05-19 23:06:10 +0800

    Functional Improvements

--- admin.py
+++ admin.py
@@ -1,16 +1,16 @@
-# from flask_admin.contrib.sqla import ModelView
-# from flask import session, redirect, url_for, request
-#
-#
-# class AdminView(ModelView):
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.static_folder = 'static'
-#
-#     def is_accessible(self):
-#         return session.get('user') == 'Administrator'
-#
-#     def inaccessible_callback(self, name, **kwargs):
-#         if not self.is_accessible():
-#             return redirect(url_for('index', next=request.url))
+from flask_admin.contrib.sqla import ModelView
+from flask import session, redirect, url_for, request
+from flask_login import current_user
+
+class AdminView(ModelView):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.static_folder = 'static'
+
+    def is_accessible(self):
+        return current_user.username == 'Administrator'
+
+    def inaccessible_callback(self, name, **kwargs):
+        if not self.is_accessible():
+            return redirect(url_for('index'))
--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 #admin authentication
-# from admin import AdminView
+from admin import AdminView
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,14 +19,14 @@ dateFile = 'data.txt'
 from app import routes, models
 from app.models import User, Topics, Polls, Options
 
-admin = Admin(app, name='Dashboard')
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Topics, db.session))
-admin.add_view(ModelView(Polls, db.session))
-admin.add_view(ModelView(Options, db.session))
+# admin = Admin(app, name='Dashboard')
+# admin.add_view(ModelView(User, db.session))
+# admin.add_view(ModelView(Topics, db.session))
+# admin.add_view(ModelView(Polls, db.session))
+# admin.add_view(ModelView(Options, db.session))
 
 #This has to do with admin authentication
-# admin = Admin(app, name='Dashboard', index_view=AdminView(User, db.session, url='/admin', endpoint='admin'))
-# admin.add_view(AdminView(Topics, db.session))
-# admin.add_view(AdminView(Polls, db.session))
-# admin.add_view(AdminView(Options, db.session))
+admin = Admin(app, name='Dashboard', index_view=AdminView(User, db.session, url='/admin', endpoint='admin'))
+admin.add_view(AdminView(Topics, db.session))
+admin.add_view(AdminView(Polls, db.session))
+admin.add_view(AdminView(Options, db.session))
